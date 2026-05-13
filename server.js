1d1233475add102f9fdd652f8ad824cfb74c5483
Jonathan Villegas <jvillegas86@gmail.com>
2026-05-12 22:06:31 -0300

    last learning activityweek  2-  ending step 4.- Needs correction, organizations do not display on the console

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -48,6 +49,14 @@ app.get('/projects', (req, res) => {
 app.get('/categories', (req, res) => {
     const title = 'Categories';
     res.render('categories', {title});
+});
+
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    console.log(organizations);
+      
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
 });
 
 app.listen(PORT, async () => {
